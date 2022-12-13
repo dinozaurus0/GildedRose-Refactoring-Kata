61ee4b593053735108b124f51c048537baa320e3
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2022-12-13 16:52:40 +0200

    Added tests for regular items when one day passed, when sell date passed and when quality reaches zero. Added `ItemExtension` to make it conform to `Equtable` for easier assertions.

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -1,11 +1,59 @@
 @testable import GildedRose
 import XCTest
 
-class GildedRoseTests: XCTestCase {
-    func testFoo() throws {
-        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
-        let app = GildedRose(items: items)
-        app.updateQuality()
-        XCTAssertEqual(app.items[0].name, "fixme")
+internal final class GildedRoseTests: XCTestCase {
+}
+
+
+// MARK: - Regular Items Tests
+extension GildedRoseTests {
+    func test_updateQuality_withRegularItem_withOneDayPassed_shouldDecreaseSellInAndQualityWithOne() {
+        // given
+        let items = [createItem(name: "Regular Element", sellIn: 10, quality: 10)]
+        let sut = makeSUT(items: items)
+
+        // when
+        sut.updateQuality()
+
+        // then
+        let expectedResult = [createItem(name: "Regular Element", sellIn: 9, quality: 9)]
+        XCTAssertEqual(sut.items, expectedResult)
+    }
+
+    func test_updateQuality_withRegularItem_withSellDatePassed_shouldDecreaseSellInWithOneAndQualityWithTwo() {
+        // given
+        let items = [createItem(name: "Regular Element", sellIn: 0, quality: 10)]
+        let sut = makeSUT(items: items)
+
+        // when
+        sut.updateQuality()
+
+        // then
+        let expectedResult = [createItem(name: "Regular Element", sellIn: -1, quality: 8)]
+        XCTAssertEqual(sut.items, expectedResult)
+    }
+
+    func test_updateQuality_withRegularItem_withQualityReachedZero_shouldDecreaseSellInWithOneAndQualityShouldRemainZero() {
+        // given
+        let items = [createItem(name: "Regular Element", sellIn: 0, quality: 0)]
+        let sut = makeSUT(items: items)
+
+        // when
+        sut.updateQuality()
+
+        // then
+        let expectedResult = [createItem(name: "Regular Element", sellIn: -1, quality: 0)]
+        XCTAssertEqual(sut.items, expectedResult)
+    }
+}
+
+// MARK: - Helpers
+extension GildedRoseTests {
+    private func makeSUT(items: [Item]) -> GildedRose {
+        GildedRose(items: items)
+    }
+
+    private func createItem(name: String, sellIn: Int, quality: Int) -> Item {
+        Item(name: name, sellIn: sellIn, quality: quality)
     }
 }
---
+++ swift/Tests/GildedRoseTests/ItemExtension.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vlad Grigore Sima on 12.12.2022.
+//
+
+import Foundation
+import GildedRose
+
+extension Item: Equatable {
+    public static func == (lhs: Item, rhs: Item) -> Bool {
+        return lhs.name == rhs.name && lhs.quality == rhs.quality && lhs.sellIn == rhs.sellIn
+    }
+}
