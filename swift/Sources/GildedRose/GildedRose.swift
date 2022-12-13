a8d5bc2aa13253cd60a9f7d2f71e4aef73057099
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2022-12-13 20:18:48 +0200

    Removed references to `Aged Brie` item from the main logic of the app.

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -1,20 +1,20 @@
 public class GildedRose {
     public var items: [Item]
     private let itemsQualityCalculators: [ItemQualityCalculator]
-
+    
     public init(items: [Item]) {
         self.items = items
         self.itemsQualityCalculators = []
     }
-
+    
     public init(items: [Item], itemsQualityCalculators: [ItemQualityCalculator]) {
         self.items = items
         self.itemsQualityCalculators = itemsQualityCalculators
     }
-
+    
     public func updateQuality() {
         for i in 0 ..< items.count {
-            if items[i].name != "Aged Brie", items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+            if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
                 if items[i].quality > 0 {
                     if items[i].name != "Sulfuras, Hand of Ragnaros" {
                         items[i].quality = items[i].quality - 1
@@ -23,14 +23,14 @@ public class GildedRose {
             } else {
                 if items[i].quality < 50 {
                     items[i].quality = items[i].quality + 1
-
+                    
                     if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
                         if items[i].sellIn < 11 {
                             if items[i].quality < 50 {
                                 items[i].quality = items[i].quality + 1
                             }
                         }
-
+                        
                         if items[i].sellIn < 6 {
                             if items[i].quality < 50 {
                                 items[i].quality = items[i].quality + 1
@@ -39,33 +39,27 @@ public class GildedRose {
                     }
                 }
             }
-
+            
             if items[i].name != "Sulfuras, Hand of Ragnaros" {
                 items[i].sellIn = items[i].sellIn - 1
             }
-
+            
             if items[i].sellIn < 0 {
-                if items[i].name != "Aged Brie" {
-                    if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].quality > 0 {
-                            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                items[i].quality = items[i].quality - 1
-                            }
+                if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+                    if items[i].quality > 0 {
+                        if items[i].name != "Sulfuras, Hand of Ragnaros" {
+                            items[i].quality = items[i].quality - 1
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
-                    if items[i].quality < 50 {
-                        items[i].quality = items[i].quality + 1
-                    }
+                    items[i].quality = items[i].quality - items[i].quality
                 }
             }
         }
-
+        
         updateItemsQuality()
     }
-
+    
     private func updateItemsQuality() {
         itemsQualityCalculators.forEach { itemQualityCalculator in
             itemQualityCalculator.computeQuality()
