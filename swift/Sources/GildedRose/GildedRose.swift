f6870d8073e3db1bd088261e01acba30151b8c76
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2022-12-14 18:44:38 +0200

    Removed code that was linked with `Aged Brie` and `Regular Item` from `GildedRose`.

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -14,27 +14,19 @@ public class GildedRose {
     
     public func updateQuality() {
         for i in 0 ..< items.count {
-            if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                if items[i].quality > 0 {
-                    if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if items[i].quality < 50 {
-                    items[i].quality = items[i].quality + 1
-                    
-                    if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].sellIn < 11 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
+            if items[i].quality < 50 {
+                items[i].quality = items[i].quality + 1
+                
+                if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+                    if items[i].sellIn < 11 {
+                        if items[i].quality < 50 {
+                            items[i].quality = items[i].quality + 1
                         }
-                        
-                        if items[i].sellIn < 6 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
+                    }
+                    
+                    if items[i].sellIn < 6 {
+                        if items[i].quality < 50 {
+                            items[i].quality = items[i].quality + 1
                         }
                     }
                 }
@@ -45,13 +37,7 @@ public class GildedRose {
             }
             
             if items[i].sellIn < 0 {
-                if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                    if items[i].quality > 0 {
-                        if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                            items[i].quality = items[i].quality - 1
-                        }
-                    }
-                } else {
+                if items[i].name != "Aged Brie" {
                     items[i].quality = items[i].quality - items[i].quality
                 }
             }
